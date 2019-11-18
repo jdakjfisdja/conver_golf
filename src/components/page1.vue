4db34f838ccde56df40042fe5e2ce1b25fafe8ef
jdakjfisdja <firebomber7777777@icloud.com>
2019-11-18 14:19:12 +0900

    second commit

---
+++ src/components/page1.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>
+            スタートページ
+        </p>
+        <router-link to= "/page2"> 写真撮影 </router-link>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/page2.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <video ref="video" id="video" autoplay playinline></video>
+    <p>カメラ撮影ページ</p>
+    <router-link to ="/">戻る</router-link>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'app',
+  data () {
+    return {
+      video: {}
+    }
+  },
+  mounted () {
+    const medias = { audio: false,
+      video: {
+        facingMode: {
+          exact: 'environment'
+        }
+      }}
+    this.video = this.$refs.video
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(medias).then(stream => {
+        this.video.srcObject = stream
+      }) // .catch(err =>{
+      // console.log(err)
+    // })
+    }
+  }
+}
+</script>
+
+<style>
+body{
+    background-color: #000000;
+    margin: 0px;
+}
+#app{
+    text-align: center;
+    color: #2c3e50;
+}
+
+#video{
+    background-color: #000000;
+    display: block;
+    width: 50%;
+}
+
+#canvas{
+    display:none;
+    width: 50%;
+}
+
+</style>
